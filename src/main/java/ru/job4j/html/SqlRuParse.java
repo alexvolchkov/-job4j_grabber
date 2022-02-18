bb209b17eda3241ce16728eefeb338b332ee0eec
alexvolchkov <volkaa@mail.ru>
2022-02-18 22:17:24 +0300

    2.1.1. Парсинг https://www.sql.ru/forum/job-offers/3 [#285210]

--- src/main/java/ru/job4j/html/SqlRuParse.java
+++ src/main/java/ru/job4j/html/SqlRuParse.java
@@ -10,13 +10,17 @@ import java.io.IOException;
 public class SqlRuParse {
 
     public static void main(String[] args) throws IOException {
-        Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers").get();
-        Elements row = doc.select(".postslisttopic");
-        for (Element td : row) {
-            Element href = td.child(0);
-            System.out.println(href.attr("href"));
-            System.out.println(href.text());
-            System.out.println(td.parent().child(5).text());
+        String url = "https://www.sql.ru/forum/job-offers/";
+        for (int i = 1; i <= 5; i++) {
+            Document doc = Jsoup.connect(String.format("%s%s", url, i)).get();
+            Elements row = doc.select(".postslisttopic");
+            for (Element td : row) {
+                Element href = td.child(0);
+                System.out.println(href.attr("href"));
+                System.out.println(href.text());
+                System.out.println(td.parent().child(5).text());
+            }
         }
+
     }
 }
