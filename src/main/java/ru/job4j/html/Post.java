dcab5fd1e71cbe4836b5f2799e22de4bad0bcba8
alexvolchkov <volkaa@mail.ru>
2022-02-18 22:30:55 +0300

    2.2. Модель данных - Post. [#285211]

---
+++ src/main/java/ru/job4j/html/Post.java
@@ -0,0 +1,42 @@
+package ru.job4j.html;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Objects;
+
+public class Post {
+    private static final DateTimeFormatter DTF = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+    private int id;
+    private String title;
+    private String description;
+    private LocalDateTime created;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Post post = (Post) o;
+        return id == post.id
+                && Objects.equals(title, post.title)
+                && Objects.equals(description, post.description);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, title, description);
+    }
+
+    @Override
+    public String toString() {
+        return "Post{"
+                + "id=" + id
+                + ", title='" + title + '\''
+                + ", description='" + description + '\''
+                + ", created=" + created.format(DTF)
+                + '}';
+    }
+}
