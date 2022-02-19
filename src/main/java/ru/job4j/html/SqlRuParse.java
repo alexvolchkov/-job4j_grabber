3303f5c1070eb1fa8d4b92f6d64ad213339ceb24
alexvolchkov <volkaa@mail.ru>
2022-02-19 16:55:14 +0300

    2.4. SqlRuParse [#285213]

--- src/main/java/ru/job4j/html/SqlRuParse.java
+++ src/main/java/ru/job4j/html/SqlRuParse.java
@@ -4,12 +4,21 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
-import ru.job4j.grabber.utils.SqlRuDateTimeParser;
+import ru.job4j.grabber.Parse;
+import ru.job4j.grabber.utils.DateTimeParser;
 
 import java.io.IOException;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
-public class SqlRuParse {
+public class SqlRuParse implements Parse {
+
+    private final DateTimeParser dateTimeParser;
+
+    public SqlRuParse(DateTimeParser dateTimeParser) {
+        this.dateTimeParser = dateTimeParser;
+    }
 
     public static void main(String[] args) throws IOException {
         String url = "https://www.sql.ru/forum/job-offers/";
@@ -25,8 +34,14 @@ public class SqlRuParse {
         }
     }
 
-    public static Post parsePage(String link) throws IOException {
-        Document doc = Jsoup.connect(link).get();
+    @Override
+    public Post detail(String link) {
+        Document doc = null;
+        try {
+            doc = Jsoup.connect(link).get();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         Element parent = doc.getElementsByClass("msgTable").get(0);
         String title = parent
                 .getElementsByClass("messageHeader")
@@ -34,8 +49,24 @@ public class SqlRuParse {
                 .childNode(1)
                 .toString().trim().split("&nbsp;")[1];
         String description = parent.getElementsByClass("msgBody").get(1).text();
-        LocalDateTime created = new SqlRuDateTimeParser().parse(
-                parent.getElementsByClass("msgBody").get(1).text());
+        LocalDateTime created = dateTimeParser.parse(
+                parent.getElementsByClass("msgFooter").get(0)
+                        .childNode(0).toString().trim().split("&")[0]);
         return new Post(title, link, description, created);
     }
+
+    @Override
+    public List<Post> list(String link) {
+        List<Post> rsl = new ArrayList<>();
+        Document doc = null;
+        try {
+            doc = Jsoup.connect(link).get();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Elements row = doc.select(".postslisttopic");
+        row.forEach(el -> rsl.add(detail(el.child(0).attr("href"))));
+        return rsl;
+    }
+
 }
