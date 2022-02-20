7793d7bbc8110f22052b4bbaf79db3cd34339f29
alexvolchkov <volkaa@mail.ru>
2022-02-20 19:04:23 +0300

    2. SQL Select by company [#2760 #267290]

--- db/scripts/query_002.sql
+++ db/scripts/query_002.sql
@@ -1,8 +1,9 @@
-select c.name, cp.count_person, find_max.max_person from company c
+select c.name, cp.count_person from company c
 inner join
 (select count(id) AS count_person, company_id from person
 group by company_id) cp on c.id = cp.company_id
-cross join
+inner join
 (select max(A.count_person) as max_person from
 (select count(id) AS count_person, company_id from person
-group by company_id) A) find_max;
+group by company_id) A) find_max
+on cp.count_person = find_max.max_person;
