e097fa152242419f5b2bd6aba25b33d4915325db
alexvolchkov <volkaa@mail.ru>
2022-02-20 11:52:22 +0300

    5. PsqlStore [#285209]

---
+++ src/main/java/ru/job4j/grabber/PsqlStore.java
@@ -0,0 +1,122 @@
+package ru.job4j.grabber;
+
+import ru.job4j.grabber.utils.SqlRuDateTimeParser;
+import ru.job4j.html.Post;
+import ru.job4j.html.SqlRuParse;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+public class PsqlStore implements Store, AutoCloseable {
+
+    private Connection cnn;
+
+    public PsqlStore(Properties cfg) {
+        try {
+            Class.forName(cfg.getProperty("jdbc.driver"));
+             this.cnn = DriverManager.getConnection(
+                    cfg.getProperty("jdbc.url"),
+                    cfg.getProperty("jdbc.username"),
+                    cfg.getProperty("jdbc.password")
+            );
+        } catch (Exception e) {
+            throw new IllegalStateException(e);
+        }
+    }
+
+    @Override
+    public void close() throws Exception {
+        if (cnn != null) {
+            cnn.close();
+        }
+    }
+
+    @Override
+    public void save(Post post) {
+        try (PreparedStatement ps = cnn.prepareStatement("insert into post (name, text, link, created) "
+                + "values (?, ?, ?, ?) on conflict (link) do nothing;",
+                Statement.RETURN_GENERATED_KEYS)) {
+            ps.setString(1, post.getTitle());
+            ps.setString(2, post.getDescription());
+            ps.setString(3, post.getLink());
+            ps.setTimestamp(4, Timestamp.valueOf(post.getCreated()));
+            ps.execute();
+            try (ResultSet resultSet = ps.getGeneratedKeys()) {
+                if (resultSet.next()) {
+                    post.setId(resultSet.getInt(1));
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public List<Post> getAll() {
+        List<Post> rsl = new ArrayList<>();
+        try (PreparedStatement ps = cnn.prepareStatement("select * from post;")) {
+            try (ResultSet resultSet = ps.executeQuery()) {
+                while (resultSet.next()) {
+                    rsl.add(createPost(resultSet));
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return rsl;
+    }
+
+    @Override
+    public Post findById(int id) {
+        Post rsl = null;
+        try (PreparedStatement ps = cnn.prepareStatement("select * from post where id = ?;")) {
+            ps.setInt(1, id);
+            try (ResultSet resultSet = ps.executeQuery()) {
+                if (resultSet.next()) {
+                    rsl = createPost(resultSet);
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return rsl;
+    }
+
+    private Post createPost(ResultSet resultSet) throws SQLException {
+        return new Post(
+                resultSet.getInt("id"),
+                resultSet.getString("name"),
+                resultSet.getString("link"),
+                resultSet.getString("text"),
+                resultSet.getTimestamp("created").toLocalDateTime()
+        );
+    }
+
+    public static void main(String[] args) {
+        Properties cfg = new Properties();
+        try (FileReader fr = new FileReader("src/main/resources/rabbit.properties")) {
+            cfg.load(fr);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        PsqlStore psqlStore = new PsqlStore(cfg);
+        SqlRuParse sqlRuParse = new SqlRuParse(new SqlRuDateTimeParser());
+        for (Post post : sqlRuParse.list("https://www.sql.ru/forum/job-offers/")) {
+            psqlStore.save(post);
+        }
+        System.out.println("Вывод всех элементов базы");
+        for (Post post : psqlStore.getAll()) {
+            System.out.println(post);
+        }
+        System.out.println("_____________________________");
+        System.out.println("Поиск по id");
+        System.out.println(psqlStore.findById(2));
+    }
+}
